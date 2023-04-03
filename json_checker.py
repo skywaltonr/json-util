61f43747a2413f1f2328a33f2719d007b68ebb18
Ryan Walton <ryanwalton1999@gmail.com>
2023-04-03 15:07:03 +0100

    something

--- json_checker.py
+++ json_checker.py
@@ -1,24 +1,19 @@
 import json
 from glob import glob
-import time
 import sys
 from json_util import JsonUtil
-import os
-print ()
 JSON_KEY = "TimecodeBurnin"
 SOURCE = sys.argv[1]
 
-st = time.time()
-output = []
 util = JsonUtil()
+success = True
 for file_name in glob(SOURCE):
     with open(file_name, mode='r', encoding='utf-8') as profile:
         profile_json = json.load(profile)
         if util.contains_key(profile_json, JSON_KEY):
-            output.append(file_name)
+            success = False
+            print(file_name)
         print(util.get_first_value(profile_json, "Queue"))
 
-et = time.time()
-sys.exit(output)
-elapsed_time = et - st
-print('Execution time:', elapsed_time * 1000, 'ms')
+if success is False:
+    sys.exit("Timecode Check Failed")
