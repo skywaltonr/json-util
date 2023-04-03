419656e6de803d0f97ff7dbdf776225d136456df
Ryan Walton <ryanwalton1999@gmail.com>
2023-04-03 15:34:28 +0100

    output update

--- json_checker.py
+++ json_checker.py
@@ -2,6 +2,7 @@ import json
 from glob import glob
 import sys
 from json_util import JsonUtil
+
 JSON_KEY = "TimecodeBurnin"
 SOURCE = sys.argv[1]
 
@@ -12,9 +13,10 @@ for file_name in glob(SOURCE):
         profile_json = json.load(profile)
         if util.contains_key(profile_json, JSON_KEY):
             files_to_fix.append(file_name)
-            print(JSON_KEY, "Found In", file_name)
+        #TODO: Check environment matches
 
 if files_to_fix:
     print("The following files failed: ")
     print(*files_to_fix, sep = "\n")
+    print()
     sys.exit("Timecode Check Failed")
